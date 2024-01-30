59dacbaa38136b432851c87122be98ec5e826269
Alan Salam <alansalamm55@gmail.com>
2024-01-30 21:23:52 +0300

    playerraycast

--- Assets/Scripts/Player/PlayerRaycast.cs
+++ Assets/Scripts/Player/PlayerRaycast.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerRaycast : MonoBehaviour
 {
+    [Header("Raycast")]
+    [SerializeField] private float rayLenght = 5f;
+    private Camera cam;
+
+    [Header("Raycast")]
+    [SerializeField] private Image corsshair;
+
+
+    //TODO: make an interface called Interactable. in it everything the player can interact with implements said interface
+    // Notecontroller is one of them but we will also have pickup objects and inspect objects they all inheret from this interface
+
+    // private NoteController noteController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
