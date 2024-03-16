b1fa8b7774749591581839827d7a432a42edc0dd
Alan Salam <alansalamm55@gmail.com>
2024-03-16 14:12:16 +0300

    pause

--- Assets/Scripts/PauseScreen.cs
+++ Assets/Scripts/PauseScreen.cs
@@ -2,13 +2,23 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseScreen : MonoBehaviour
 {
     [SerializeField] private FirstPersonController playerController;
     [SerializeField] RectTransform canvas;
-
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button quitButton;
     private bool isPaused = false;
+    private void Start()
+    {
+        canvas.gameObject.SetActive(false);
+
+        continueButton.onClick.AddListener(TogglePause);
+        quitButton.onClick.AddListener(QuitGame);
+        continueButton.Select();
+    }
 
     private void TogglePause()
     {
@@ -48,6 +58,10 @@ public class PauseScreen : MonoBehaviour
         playerController.enabled = true;
         playerController.GetPlayerCameraRoot().GetComponent<PlayerBookComponent>().LockBookVisual(false);
         playerController.GetPlayerCameraRoot().GetComponent<PlayerRaycast>().ShowCrossHair(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     void Update()
