54359a9495bcb8ec51840184c5671fbf2a5992da
Alan Salam <alansalamm55@gmail.com>
2024-03-16 14:59:15 +0300

    fade 2

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,25 +1,26 @@
-using StarterAssets;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using StarterAssets;
 
 public class StartScreen : MonoBehaviour
 {
     [SerializeField] private Transform startPosition;
     [SerializeField] private FirstPersonController playerController;
+    [SerializeField] private Image fadeImage; // Image component for the fade effect
+    [SerializeField] private float fadeDuration = 0.5f; // Duration for the fade effect
+    [SerializeField] private float playerMoveTime = 1.0f;
 
     [SerializeField] private Button startButton;
     [SerializeField] private Button quitButton;
-    [SerializeField] RectTransform canvas;
-    [SerializeField] PauseScreen pauseScreen;
-    [SerializeField] private float playerMoveTime = 1.0f;
+    [SerializeField] private RectTransform canvas;
+    [SerializeField] private PauseScreen pauseScreen;
 
     public event Action onGameStart;
 
     private void Start()
     {
         canvas.gameObject.SetActive(true);
-
         // Enable cursor and unlock it
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
@@ -34,6 +35,29 @@ public class StartScreen : MonoBehaviour
         startButton.onClick.AddListener(StartGame);
         quitButton.onClick.AddListener(QuitGame);
         startButton.Select();
+
+        fadeImage.gameObject.SetActive(true);
+        FadeInStartScreen();
+    }
+
+    private void FadeInStartScreen()
+    {
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            Color startColor = fadeImage.color;
+            Color endColor = new Color(0f, 0f, 0f, 0f); // Fully transparent color
+
+            LeanTween.value(fadeImage.gameObject, startColor, endColor, fadeDuration)
+                .setOnUpdate((Color value) =>
+                {
+                    fadeImage.color = value;
+                })
+                .setOnComplete(() =>
+                {
+                    fadeImage.gameObject.SetActive(false);
+                });
+        }
     }
 
     public void StartGame()
